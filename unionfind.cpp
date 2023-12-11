7d5bd28414a1263f887718847f5dfe4ccb6c7395
LeonardoValente-unicu <l197899@dac.unicamp.br>
2023-12-11 13:54:27 -0300

    changing all files to .cpp, adding union find

---
+++ unionfind.cpp
@@ -0,0 +1,44 @@
+/*
+from https://github.com/defnotmee/definitely-not-a-lib
+
+why am i doing this
+*/
+
+#ifndef O_O
+#include"header.h"
+#endif
+
+
+struct UnionFind{
+    vector<int> pai;
+
+    UnionFind(int n = 0){
+        pai = vector<int>(n);
+        iota(all(pai),0);
+    }
+
+    int find(int id){
+        if(pai[id] == id)
+            return id;
+        return pai[id] = find(pai[id]);
+    }
+
+    // Returns 1 if a and b were in different groups.
+    // Useful for Kruskal.
+    bool onion(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if(a == b)
+            return 0;
+
+        // Here is where you edit how to merge nodes
+        pai[a] = b;
+        
+        return 1;
+    }
+
+    bool same(int a, int b){
+        return find(a) == find(b);
+    }
+};
