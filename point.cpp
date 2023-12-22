d342da484aacfdee7eca5edb33f9dd10cba99f6f
LeonardoValente-unicu <l197899@dac.unicamp.br>
2023-12-22 03:09:11 -0300

    Adding point (for geometry)

---
+++ point.cpp
@@ -0,0 +1,40 @@
+/*
+from https://github.com/defnotmee/definitely-not-a-lib
+*/
+
+#ifndef O_O
+#include"header.h"
+#endif
+
+template<typename T = double>
+struct point{
+    T x, y;
+
+    inline point operator+(point b){
+        return {x+b.x, y+b.y};
+    }
+
+    inline point operator-(point b){
+        return {x-b.x, y-b.y};
+    }
+
+    inline point operator*(T scale){
+        return {x*scale, y*scale};
+    }
+
+    inline T cross(point b){
+        return x*b.y-b.x*y;
+    }
+
+    inline T dot(point b){
+        return x*b.x + y*b.y;
+    }
+
+    inline T dist2(){
+        return x*x+y*y;
+    }
+
+    inline T dist(){
+        return sqrt(dist2());
+    }
+};
