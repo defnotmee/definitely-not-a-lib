cb1435ce36543202c3c4669eb27e1dfa9502476e
LeonardoValente-unicu <l197899@dac.unicamp.br>
2024-01-05 22:24:50 -0300

    Adding useful pragmas

---
+++ pragmas.cpp
@@ -0,0 +1,12 @@
+/*
+from https://github.com/defnotmee/definitely-not-a-lib
+
+Useful pragmas from nor's blog: https://codeforces.com/blog/entry/96344
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+// Pragma for randomized solutions by magnus.hegdahl
+
+#pragma VODOO magic("Please work this time")
