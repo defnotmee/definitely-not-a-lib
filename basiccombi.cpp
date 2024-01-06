824eafa2f1b057b9c01c8cbf1b54f0c2697b7813
LeonardoValente-unicu <l197899@dac.unicamp.br>
2024-01-06 01:23:46 -0300

    adding linear sieve + changing how basic combi works

--- basiccombi.cpp
+++ basiccombi.cpp
@@ -1,8 +1,9 @@
 /*
 from https://github.com/defnotmee/definitely-not-a-lib
 
-Implements factorials and binomials modulo p for all 
-numbers from 0 to n-1.
+Calculates factorials and binomials modulo p for all 
+numbers from 0 to n-1. By default creates the struct
+for n = MAXN and names it combi.
 
 Idea for O(n) inverse of each number from this blog:
 https://codeforces.com/blog/entry/83075
@@ -18,7 +19,7 @@ struct Combi{
     // note that inv[0] = 1 in this impl
     vector<modint> fac, inv, invfac;
 
-    Combi(int n = 0){
+    Combi(int n = MAXN){
         fac = inv = invfac = vector<modint>(n,1);
 
         for(int i = 2; i < n; i++){
@@ -31,4 +32,4 @@ struct Combi{
     modint choose(int n, int k){
         return fac[n]*invfac[k]*invfac[n-k];
     }
-};
+} combi;
---
+++ linearsieve.cpp
@@ -0,0 +1,54 @@
+/*
+from https://github.com/defnotmee/definitely-not-a-lib
+
+Calculates smallest prime that divides each number for
+all x < n and also maintains a list of all primes up to that
+in O(n)
+
+By default creates a sieve named sieve of size MAXN.
+*/
+
+#ifndef O_O
+#include"header.h"
+#endif
+
+struct Sieve{
+    vector<int> primes;
+    vector<int> next;
+
+    Sieve(int n = MAXN){
+        next = vector<int>(MAXN);
+
+        for(int i = 2; i < MAXN; i++){
+            if(!next[i])
+                next[i] = i, primes.push_back(i);
+            
+            for(ll j : primes){
+                if(j*i >= n)
+                    break;
+                next[j*i] = j;
+                if(j == next[i])
+                    break;
+            }
+        }
+    }
+
+    inline bool is_prime(int n){
+        return next[n] == n;
+    }
+
+    // returns pairs in form {prime, exponent}
+    // will always return them in ascending order
+    vector<pii> factorize(int n){
+        vector<pii> ret;
+
+        while(n != 1){
+            int p = next[n];
+            int ct = 0;
+            while(n%p == 0)
+                ct++, n/=p;
+            ret.push_back({p,ct});
+        }
+        return ret;
+    }
+} sieve;
