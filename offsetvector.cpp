83f774331c937612abcadf2b55fe4cbdbf97d96f
LeonardoValente-unicu <l197899@dac.unicamp.br>
2024-01-11 12:03:48 -0300

    adding vector with offset

---
+++ offsetvector.cpp
@@ -0,0 +1,25 @@
+/*
+from https://github.com/defnotmee/definitely-not-a-lib
+
+Create a vector that can be accessed with indexes from [-n to n-1].
+*/
+
+#ifndef O_O
+#include"header.h"
+#endif
+
+template<typename T>
+struct offvec{
+
+    vector<T> v;
+    int offset;
+
+    offvec(int n = 0, T def = T()){
+        offset = n;
+        v = vector<T>(2*n, def);
+    }
+
+    T& operator[](int id){
+        return v[id+offset];
+    }
+};
