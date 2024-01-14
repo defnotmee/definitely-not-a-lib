a50d2b252bb5a2c0fbdd7e9e2c113d29639f72a9
LeonardoValente-unicu <l197899@dac.unicamp.br>
2024-01-13 23:42:29 -0300

    Taking references off stuff for easier use

--- vector++.cpp
+++ vector++.cpp
@@ -12,16 +12,18 @@ get their scalar product via operator overloads.
 #include"header.h"
 #endif
 
+
 template<typename T>
-constexpr vector<T> operator+(vector<T>& a, vector<T> b){
+constexpr vector<T> operator+(vector<T> a, vector<T> b){
     vector<T> ret = a;
     for(int i = 0; i < a.size(); i++)
         ret[i]+=b[i];
     return ret;
 }
 
+
 template<typename T>
-constexpr vector<T> operator*(vector<T>& a, T b){
+constexpr vector<T> operator*(vector<T> a, T b){
     vector<T> ret = a;
     for(int i = 0; i < a.size(); i++)
         ret[i]*=b;
@@ -29,22 +31,22 @@ constexpr vector<T> operator*(vector<T>& a, T b){
 }
 
 template<typename T>
-constexpr vector<T> operator*(T b, vector<T>& a){
+constexpr vector<T> operator*(T b, vector<T> a){
     return a*b;
 }
 
 template<typename T>
-constexpr vector<T> operator/(vector<T>& a, T b){
+constexpr vector<T> operator/(vector<T> a, T b){
     return a*(T(1)/b);
 }
 
 template<typename T>
-constexpr vector<T> operator-(vector<T>& a, vector<T> b){
+constexpr vector<T> operator-(vector<T> a, vector<T> b){
     return a+(b*T(-1));
 }
 
 template<typename T>
-constexpr T operator*(vector<T>& a, vector<T> b){
+constexpr T operator*(vector<T> a, vector<T> b){
     T ret = T(0);
     for(int i = 0; i < a.size(); i++)
         ret+=a[i]*b[i];
@@ -53,21 +55,21 @@ constexpr T operator*(vector<T>& a, vector<T> b){
 
 
 template<typename T>
-void operator+=(vector<T>& a, vector<T> b){
+void operator+=(vector<T> a, vector<T> b){
     a = a + b;
 }
 
 template<typename T>
-void operator*=(vector<T>& a, T b){
+void operator*=(vector<T> a, T b){
     a = a * b;
 }
 
 template<typename T>
-void operator/=(vector<T>& a, T b){
+void operator/=(vector<T> a, T b){
     a = a / b;
 }
 
 template<typename T>
-void operator-=(vector<T>& a, vector<T> b){
+void operator-=(vector<T> a, vector<T> b){
     a = a - b;
 }
