caaf09ec1755d29d40b871ef4e564ed12b72da4b
LeonardoValente-unicu <l197899@dac.unicamp.br>
2024-01-16 13:33:08 -0300

    Adding inverse functionality + fixing bug where i took modulo twice. Oops

--- modint.cpp
+++ modint.cpp
@@ -14,29 +14,45 @@ struct modint{
     ll x;
 
     // It is assumed 0 <= v. Extra mod is taken for safety, 
-    // feel free to comment if you think it will inpact runtime
+    // feel free to use the other constructor if you think it inpacts performance
     // (it won't)
-    modint(ll v = 0){ 
-        x = (v+MOD)%MOD;
-    }
+    modint(ll v = 0) : x((v+MOD)%MOD){};
+
+    modint(ll v, ll raw) : x(v){};
 
     // only on C++20
     bool operator<=>(const modint&) const = default;
 
     modint operator+(modint b){
-        return {x+b.x - MOD*(x+b.x >= MOD)};
+        return modint(x+b.x - MOD*(x+b.x >= MOD),1);
     }
 
     modint operator-(modint b){
-        return {x-b.x + MOD*(x-b.x < 0)};
+        return modint(x-b.x + MOD*(x-b.x < 0),1);
     }
 
     modint operator*(modint b){
-        return {(x*b.x%MOD)};
+        return modint((x*b.x%MOD),1);
     };
 
+    modint inverse(){
+        int x = this->x, y = MOD;
+
+        complex<int> cx = {1,0}, cy = {0,1};
+
+        while(x){
+            int curdiv = y/x;
+            y-=curdiv*x;
+            cy-=curdiv*cx;
+            swap(cx, cy);
+            swap(x, y);
+        }
+
+        return modint(cy.real());
+    }
+
     modint operator/(modint b){
-        return *this*power(b,MOD-2);
+        return *this*b.inverse();
     }
 
     void operator+=(modint b){
