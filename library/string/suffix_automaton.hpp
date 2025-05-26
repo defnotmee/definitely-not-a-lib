4c06d3a4bf5379dbc44e1369194e728b0cc348b9
defnotmee <leonardovncomp@gmail.com>
2025-05-26 16:04:25 -0300

    updating suffix automaton a bit

--- library/string/suffix_automaton.hpp
+++ library/string/suffix_automaton.hpp
@@ -8,17 +8,18 @@
 template<int ALPHA = 26, int INI = 'a'>
 struct SuffixAutomaton {
     struct state{
-        int len, link;
+        int len, firstpos, link = -1, is_clone = 0;
         array<int, ALPHA> ptr;
 
-        state(int len = 0, int link = -1) : len(len), link(link){
+        state(int len = 0, int firstpos = -1)
+            : len(len), firstpos(firstpos){
             fill(all(ptr),-1);
         }
     };
 
     vector<state> sa;
     int last = 0;
-
+    int ct = 0;
     SuffixAutomaton(){
         sa = {{}};
     }
@@ -26,7 +27,7 @@ struct SuffixAutomaton {
     void extend(int c){
         c-=INI;
         int nxt = sa.size();
-        sa.push_back({sa[last].len+1});
+        sa.push_back({sa[last].len+1, ct++});
 
         int pivot = last;
 
@@ -41,6 +42,7 @@ struct SuffixAutomaton {
                 int nlink = sa[nxt].link = sa.size();
                 sa.push_back(sa[tmp]);
                 sa.back().len = sa[pivot].len+1;
+                sa.back().is_clone = 1;
                 int tmp = sa[pivot].ptr[c];
                 sa[tmp].link = nlink;
                 
@@ -58,6 +60,7 @@ struct SuffixAutomaton {
         sa.reserve(2*s.size());
         for(char i : s)
             extend(i);
+
     }
 
     bool accept(string s){
@@ -71,4 +74,31 @@ struct SuffixAutomaton {
         return 1;
     }
 
+    vector<int> get_ct(){
+        vector<int> ct(sa.size());
+        vector<bstring<int>> rslink(sa.size());
+
+        for(int i = 1; i < sa.size(); i++){
+            rslink[sa[i].link].push_back(i);
+        }
+
+        auto dfs =[&](int id, auto && dfs) -> void {
+            ct[id] = !sa[id].is_clone;
+
+            for(int i : rslink[id])
+                dfs(i,dfs), ct[id]+=ct[i];
+        };
+
+        dfs(0,dfs);
+
+        return ct;
+    }
+
+    state& operator[](int id){
+        return sa[id];
+    }
+
+    int size(){
+        return sa.size();
+    }
 };
